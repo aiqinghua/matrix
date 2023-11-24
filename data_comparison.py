45d0f82125ff57450f2081a97843b3efbe00376a
aiqinghua <670565197@qq.com>
2023-11-24 19:20:51 +0800

    优化

--- data_comparison.py
+++ data_comparison.py
@@ -7,7 +7,7 @@ from deepdiff import DeepDiff
 
 
 confs = read_config()
-kafka_msg_file = confs["kafka"]["kafka_http_msg"]
+kafka_msg_file = confs["kafka"]["kafka_msg"]
 raw_data_file = confs["pcap"]["config_path"]
 
 # print(f"kafka输出文件为：{kafka_msg_file}\n原始数据文件为：{raw_data_file}")
@@ -36,26 +36,30 @@ def comparison_json_field():
     field_to_compare = ["sip", "dip", "sport", "dport", "protocol", "method"]
     data1 = {field: None for field in field_to_compare}
     data2 = []
-    with open(raw_data_file, "r", encoding="utf-8") as f1:
-        line = f1.readline()
-        obj = json.loads(line)
-        for field in field_to_compare:
-            if field in obj:
-                data1[field] = obj[field]
+    for raw_data in raw_data_file:
+        with open(raw_data, "r", encoding="utf-8") as f1:
+            print(raw_data)
+            # line = f1.readline()
+            # obj = json.loads(line)
+            # for field in field_to_compare:
+            #     if field in obj:
+            #         data1[field] = obj[field]
 
-    with open(kafka_msg_file, "r", encoding="utf-8") as f2:
-        for line in f2:
-            obj = json.loads(line)
-            temp_data = {field: obj.get(field) for field in field_to_compare}
-            data2.append(temp_data)
+    for file_name in kafka_msg_file:
+        with open(file_name, "r", encoding="utf-8") as f2:
+            for line in f2:
+                print(line)
+                # obj = json.loads(line)
+                # temp_data = {field: obj.get(field) for field in field_to_compare}
+                # data2.append(temp_data)
 
-    matching_data, failed_fields = find_matching_data(data1, data2)
-    if matching_data != "":
-        print(f"文件2中与文件1匹配成功的字段为：{matching_data}")
-    else:
-        # print()
-        print(f"没有匹配的数据！")
-    if failed_fields:
-        print(f"匹配失败的字段为：{failed_fields}")
+    # matching_data, failed_fields = find_matching_data(data1, data2)
+    # if matching_data != "":
+    #     print(f"文件2中与文件1匹配成功的字段为：{matching_data}")
+    # else:
+    #     # print()
+    #     print(f"没有匹配的数据！")
+    # if failed_fields:
+    #     print(f"匹配失败的字段为：{failed_fields}")
 
 comparison_json_field()
