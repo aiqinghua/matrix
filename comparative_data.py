c5cfce64fabc86f59bc39a89af58b1ef9a87c710
aiqinghua <670565197@qq.com>
2023-12-01 20:14:59 +0800

    优化数据对比逻辑

--- comparative_data.py
+++ comparative_data.py
@@ -2,20 +2,27 @@
 # -*- coding: UTF-8 -*-
 from read_config import read_config
 
-# '''
-# 1.获取原始数据
-# 2.获取kafka数据
-# 3.原始数据与kafka中的数据进行对比
-#     原始数据为1条数据
-#     需要对比kafka的数据为1个list，里面为多条数据
-#         1.首先对比整条数据，看整条数据是否一致
-#         2.对比数据中的字段，看字段内容是否一致
-# 4.输出对比成功/失败的字段，对比时的数据
-# '''
+
+'''
+1.获取原始数据
+2.获取kafka数据
+3.原始数据与kafka中的数据进行对比
+    原始数据为1条数据
+    需要对比kafka的数据为1个list，里面为多条数据
+        1.首先对比整条数据，看整条数据是否一致
+        2.对比数据中的字段，看字段内容是否一致
+4.输出对比成功/失败的字段，对比时的数据
+5.判断每个协议的失败字段，若有1条数据失败的字段为0，则该协议对比成功
+'''
 
 
 data_comparison_logs_path = read_config()
 comparative_data_log = data_comparison_logs_path["logs"]["data_comparison_logs_path"]
+Comparison_results_log = data_comparison_logs_path["logs"]["Comparison_results"]
+
+# data1 = {'orig_mac': '00:02:3f:ec:61:11', 'resp_mac': 'ff:ff:ff:ff:ff:ff', 'sip': '195.168.186.21', 'dip': '10.132.123.191', 'dport': 80, 'sport': 13888, 'service': 'http', 'protocol': 'tcp', 'method': 'GET', 'uri': 'madoc/index.php', 'request_version': '1.1', 'request_header_names': ['USER-AGENT', 'HOST', 'ACCEPT'], 'request_header_values': ['curl/7.29.0', 'claxet.co.uk', '*/*'], 'request_body': '', 'response_version': '1.1', 'status_code': 200, 'status_msg': 'OK', 'response_header_names': ['ACCEPT-RANGES', 'CACHE-CONTROL', 'CONNECTION', 'CONTENT-TYPE', 'DATE', 'ETAG', 'LAST-MODIFIED', 'PRAGMA', 'SERVER', 'SET-COOKIE'], 'response_header_values': ['bytes', 'private, no-cache, no-store, proxy-revalidate, no-transform', 'keep-alive', 'text/html', 'Thu, 30 Jul 2020 05:55:04 GMT', '\\"588604c8-94d\\"', 'Mon, 23 Jan 2017 13:27:36 GMT', 'no-cache', 'bfe/1.0.8.18', 'BDORZ=27315; max-age=86400; domain=.baidu.com; path=/'], 'response_body': 'title,keywords,description,link_tags,url_name,content,is_recommend,tags,publish_time,alert,<!DOCTYPE html>\r\n<!--STATUS OK--><html> <head><meta http-equiv=content-type content=text/html;charset=utf-8><meta http-equiv=X-UA-Compatible content=IE=Edge><meta content=always name=referrer><link rel=stylesheet type=text/css href=http://s1.bdstatic.com/r/www/cache/bdorz/baidu.mi', 'response_body_truncated': False, 'schema_type': 1, 'schema_version': 1, 'sensor_id': '4c15235536204d4cb52a81547953a2f6', 'error': 'mbuf is null'}
+# data2 = [{'ts': 1701249328586, 'sid': '6306972972800035', 'parent_sid': '', 'schema_type': 1, 'schema_version': 1, 'sensor_id': 'e5e19ce97c23456b83777ee27bcc3f17', 'id': '6306972972800036', 'vwire_name': 'abcd0d40304e274b', 'vsys_name': 'vsys_second', 'sip': '195.168.186.21', 'dip': '10.132.123.191', 'sport': 13888, 'dport': 80, 'method': 'GET', 'uri': 'madoc/index.php', 'request_version': '1', 'request_header_names': ['USER-AGENT', 'HOST', 'ACCEPT'], 'request_header_values': ['curl/7.29.0', 'claxet.co.uk', '*/*'], 'request_body_len': 0, 'request_body_truncated': False, 'request_body': '', 'response_version': '', 'status_code': 0, 'status_msg': '', 'response_header_names': [], 'response_header_values': [], 'response_body_len': 0, 'response_body_truncated': False, 'response_body': '', 'request_body_b64encoded': False, 'response_body_b64encoded': False, 'error': 'req_ack: 1, rsp_seq: 0, rsp no data, first seq: 2, expect next parser seq: 754, real next parser seq: 0'}, {'ts': 1701249349586, 'sid': '6306972972800035', 'parent_sid': '', 'schema_type': 1, 'schema_version': 1, 'sensor_id': 'e5e19ce97c23456b83777ee27bcc3f17', 'id': '6306973df4800048', 'vwire_name': 'abcd0d40304e274b', 'vsys_name': 'vsys_second', 'sip': '195.168.186.21', 'dip': '10.132.123.191', 'sport': 13888, 'dport': 80, 'method': '', 'uri': '', 'request_version': '', 'request_header_names': [], 'request_header_values': [], 'request_body_len': 0, 'request_body_truncated': False, 'request_body': '', 'response_version': '1', 'status_code': 200, 'status_msg': 'OK', 'response_header_names': ['ACCEPT-RANGES', 'CACHE-CONTROL', 'CONNECTION', 'CONTENT-TYPE', 'DATE', 'ETAG', 'LAST-MODIFIED', 'PRAGMA', 'SERVER', 'SET-COOKIE'], 'response_header_values': ['bytes', 'private, no-cache, no-store, proxy-revalidate, no-transform', 'keep-alive', 'text/html', 'Thu, 30 Jul 2020 05:55:04 GMT', '\\"588604c8-94d\\"', 'Mon, 23 Jan 2017 13:27:36 GMT', 'no-cache', 'bfe/1.0.8.18', 'BDORZ=27315; max-age=86400; domain=.baidu.com; path=/'], 'response_body_len': 0, 'response_body_truncated': False, 'response_body': '', 'request_body_b64encoded': False, 'response_body_b64encoded': False, 'error': 'req_ack: 0, rsp_seq: 2'}]
+
 
 def Entire_data(data1, data2):
     # 整条数据对比
@@ -52,42 +59,25 @@ def field_data(data1, data2, field,data1_key):
             "data1": data1,
             "keys": data1_key
         })
-        for res in result:
-            print(f"匹配协议为：{res['keys']}\t\033[34m成功的字段：{res['field_success']}\033[0m\t "
-                  f"\033[31m失败的字段：{res['field_fail']}\033[0m")
-            with open(comparative_data_log[0], "a", encoding="utf-8") as log_file:
-                log_file.write(f"匹配成功的字段：{res['field_success']}\t 匹配失败的字段：{res['field_fail']}"
-              f"\n匹配的数据：{res['line']}\n原始数据：{res['data1']}\n\n")
+    for res in result:
+        # print(f"\033[33m当前匹配协议为：{res['keys']}\033[0m\t\033[34m成功的字段：{res['field_success']}\033[0m\t "
+        #       f"\033[31m失败的字段：{res['field_fail']}\033[0m")
+
+        with open(comparative_data_log[0], "a", encoding="utf-8") as log_file:
+            log_file.write(f"匹配成功的字段：{res['field_success']}\t 匹配失败的字段：{res['field_fail']}"
+          f"\n匹配的数据：{res['line']}\n原始数据：{res['data1']}\n\n")
+        if len(res['field_fail']) < 1:
+            with open(Comparison_results_log[0], "a", encoding="utf-8") as res_file:
+                res_file.write(f"{res['keys']}协议对比成功\n")
+        else:
+            pass
+            # print(f"{res['keys']}协议对比失败")
     return result
 
-
-def Processing_comparative_data(data1,data2,field):
-    # 将对比的结果处理
-    result = []
-    for data in data2:
-        Entire_success, Entire_fail= Entire_data()
-        field_success, field_fail = field_data(field)
-        f=[]
-        f.append(field_success)
-        f.append(field_fail)
-        f.append(data)
-        f.append(data1)
-        result.append(f)
-    return result
-
-
-def get_result():
-    get_res = Processing_comparative_data()
-    dirty_data = []
-    for tmp in get_res:
-        # print(tmp)
-        # print(type(len(tmp)))
-        i = 1
-        print(f"匹配成功的字段为：{tmp[len(tmp)- (i+3)]} \n匹配失败的字段为：{tmp[len(tmp)- (i+2)]}\n匹配的数据为：{tmp[len(tmp)- (i+1)]}"
-              f"\n匹配的原始数据为：{tmp[len(tmp)-i]}\n")
 
 # field = ["sip", "dip", "protocol", "method"]
-# success, fail = field_data(field)
-
-# field_data(data1[0], data2, field)
-# get_result()
+# data1_key = "http.conf"
+# # success, fail = field_data(field)
+#
+# field_data(data1, data2, field,data1_key)
+# # get_result()
--- data_comparison.py
+++ data_comparison.py
@@ -11,6 +11,7 @@ confs = read_config()
 kafka_msg_file = confs["kafka"]["kafka_msg"]
 raw_data_file = confs["pcap"]["config_path"]
 comparative_data_log = confs["logs"]["data_comparison_logs_path"]
+Comparison_results_log = confs["logs"]["Comparison_results"]
 
 
 def comparison_json_field():
@@ -28,97 +29,113 @@ def comparison_json_field():
 
     return new_data, kafka_data
 
+
 def process_protocol_data(data1_keys, data1_value, data2_value, fields_to_compare):
-    if data1_keys == "http.conf":
-        http_field = ["protocol", "method", "uri", "status_code",  "status_msg", "request_header_names",
-                      "request_header_values", "request_body", "response_body"]
-        http_field.extend(fields_to_compare)
-        comparative_data.field_data(data1_value[0], data2_value, http_field, data1_keys)
-
-
-    elif data1_keys == "dns.conf":
-        dns_field = ["query", "qtype_name"]
-        dns_field.extend(fields_to_compare)
-        comparative_data.field_data(data1_value[0], data2_value, dns_field, data1_keys)
-    elif data1_keys == "tcp.conf":
-        tcp_field = ["orig_mac", "resp_mac", "protocol"]
-        tcp_field.extend(fields_to_compare)
-        comparative_data.field_data(data1_value[0], data2_value, tcp_field, data1_keys)
-    elif data1_keys == "udp.conf":
-        udp_field = ["orig_mac", "resp_mac", "protocol"]
-        udp_field.extend(fields_to_compare)
-        comparative_data.field_data(data1_value[0], data2_value, udp_field, data1_keys)
-    elif data1_keys == "icmp.conf":
-        icmp_field = ["sip", "dip", "is_echo", "itype", "icode", "payload_len", "ttl", "payload"]
-        comparative_data.field_data(data1_value[0], data2_value, icmp_field, data1_keys)
-
-    elif data1_keys == "smtp.conf" or data1_keys == "pop3.conf" or data1_keys == "imap.conf":
-        mail_field = ["schema_type", "protocol", "mail_from", "rcpt_to", "raw_mail"]
-        mail_field.extend(fields_to_compare)
-        comparative_data.field_data(data1_value[0], data2_value, mail_field, data1_keys)
-
-    elif data1_keys == "arp.conf":
-        arp_field = ["schema_type", "spa", "sha", "tpa", "tha", "opcode", "hardware_type", "protocol_type", "dst_mac"]
-        comparative_data.field_data(data1_value[0], data2_value, arp_field, data1_keys)
-    elif data1_keys == "dhcp.conf":
-        dhcp_field = ["schema_type", "lease_time", "trans_id", "assigned_dns", "assigned_ip", "assigned_netmask",
-                      "assigned_router", "mac"]
-        comparative_data.field_data(data1_value[0], data2_value, dhcp_field, data1_keys)
-    elif data1_keys == "ftp.conf":
-        ftp_field = ["schema_type", "user", "command", "arg", "code", "msg"]
-        ftp_field.extend(fields_to_compare)
-        comparative_data.field_data(data1_value[0], data2_value, ftp_field, data1_keys)
-    elif data1_keys == "rarp.conf":
-        rarp_field = ["schema_type", "src_mac", "dst_mac", "opcode", "hardware_type", "protocol_type", "spa", "sha",
-                      "tpa", "tha"]
-        comparative_data.field_data(data1_value[0], data2_value, rarp_field, data1_keys)
-    elif data1_keys == "syslog.conf":
-        syslog_field = ["schema_type", "facility", "level", "message"]
-        syslog_field.extend(fields_to_compare)
-        comparative_data.field_data(data1_value[0], data2_value, syslog_field, data1_keys)
-    elif data1_keys == "telnet.conf":
-        telnet_field = ["schema_type", "content"]
-        telnet_field.extend(fields_to_compare)
-        comparative_data.field_data(data1_value[0], data2_value, telnet_field, data1_keys)
-    elif data1_keys == "ssl.conf":
-        ssl_field = ["serial", "fingerprint", "server_ja3_hash", "client_ja3_hash", "schema_type", "version", "cipher","server_name",
-                     "established", "server_public_key", "subject", "issuer", "not_valid_before", "not_valid_after"]
-        ssl_field.extend(fields_to_compare)
-        comparative_data.field_data(data1_value[0], data2_value, ssl_field, data1_keys)
-    elif data1_keys == "mysql.conf":
-        mysql_field = ["schema_type","db_type", "username", "command", "arg", "success"]
-        mysql_field.extend(fields_to_compare)
-        comparative_data.field_data(data1_value[0], data2_value, mysql_field, data1_keys)
-    elif data1_keys == "socks.conf":
-        socks_field = ["schema_type", "command", "status", "addr","port"]
-        socks_field.extend(fields_to_compare)
-        comparative_data.field_data(data1_value[0], data2_value, socks_field, data1_keys)
-    elif data1_keys == "snmp.conf":
-        snmp_field = ["schema_type", "duration", "get_request_count","get_next_request_count","response_count",
+    field_data_dict = {
+        "http.conf": {
+            "fields": ["protocol", "method", "uri", "status_code", "status_msg", "request_header_names",
+                       "request_header_values", "request_body", "response_body"],
+            "default_fields": fields_to_compare
+        },
+        "dns.conf": {
+            "fields": ["query", "qtype_name"],
+            "default_fields": fields_to_compare
+        },
+        "tcp.conf": {
+            "fields": ["orig_mac", "resp_mac", "protocol"],
+            "default_fields": fields_to_compare
+        },
+        "udp.conf": {
+            "fields": ["orig_mac", "resp_mac", "protocol"],
+            "default_fields": fields_to_compare
+        },
+        "icmp.conf": {
+            "fields": ["sip", "dip", "is_echo", "itype", "icode", "payload_len", "ttl", "payload"],
+            "default_fields": fields_to_compare
+        },
+        "smtp.conf": {
+            "fields": ["schema_type", "protocol", "mail_from", "rcpt_to", "raw_mail"],
+            "default_fields": fields_to_compare
+        },
+        "pop3.conf": {
+            "fields": ["schema_type", "protocol", "mail_from", "rcpt_to", "raw_mail"],
+            "default_fields": fields_to_compare
+        },
+        "imap.conf": {
+            "fields": ["schema_type", "protocol", "mail_from", "rcpt_to", "raw_mail"],
+            "default_fields": fields_to_compare
+        },
+        "arp.conf": {
+            "fields": ["schema_type", "spa", "sha", "tpa", "tha", "opcode", "hardware_type", "protocol_type", "dst_mac"],
+            "default_fields": fields_to_compare
+        },
+        "dhcp.conf": {
+            "fields": ["schema_type", "lease_time", "trans_id", "assigned_dns", "assigned_ip", "assigned_netmask",
+                      "assigned_router", "mac"],
+            "default_fields": fields_to_compare
+        },
+        "ftp.conf": {
+            "fields": ["schema_type", "user", "command", "arg", "code", "msg"],
+            "default_fields": fields_to_compare
+        },
+        "rarp.conf": {
+            "fields": ["schema_type", "src_mac", "dst_mac", "opcode", "hardware_type", "protocol_type", "spa", "sha",
+                      "tpa", "tha"],
+            "default_fields": fields_to_compare
+        },
+        "syslog.conf": {
+            "fields": ["schema_type", "facility", "level", "message"],
+            "default_fields": fields_to_compare
+        },
+        "telnet.conf": {
+            "fields": ["schema_type", "content"],
+            "default_fields": fields_to_compare
+        },
+        "ssl.conf": {
+            "fields": ["serial", "fingerprint", "server_ja3_hash", "client_ja3_hash", "schema_type", "version", "cipher","server_name",
+                     "established", "server_public_key", "subject", "issuer", "not_valid_before", "not_valid_after"],
+            "default_fields": fields_to_compare
+        },
+        "mysql.conf": {
+            "fields": ["schema_type","db_type", "username", "command", "arg", "success"],
+            "default_fields": fields_to_compare
+        },
+        "socks.conf": {
+            "fields": ["schema_type", "command", "status", "addr","port"],
+            "default_fields": fields_to_compare
+        },
+        "snmp.conf": {
+            "fields": ["schema_type", "duration", "get_request_count","get_next_request_count","response_count",
                       "set_request_count","trap_count", "get_bulk_request_count", "inform_request_count", "sys_desc", "sys_uptime",
-                      "sys_contact", "sys_name", "sys_memory_size", "sys_if_number"]
-        snmp_field.extend(fields_to_compare)
-        comparative_data.field_data(data1_value[0], data2_value, snmp_field, data1_keys)
-    elif data1_keys == "llmnr.conf":
-        llmnr_field = ["schema_type", "query", "qtype", "qtype_name", "rcode_name", "answers", "answer_queries", "rejected", "rcode",
-                       "answer_types"]
-        llmnr_field.extend(fields_to_compare)
-        comparative_data.field_data(data1_value[0], data2_value, llmnr_field, data1_keys)
-    elif data1_keys == "ldap.conf":
-        ldap_field = ["schema_type", "op", "req_dn", "res_code", "matched_dn", "res_error"]
-        ldap_field.extend(fields_to_compare)
-        comparative_data.field_data(data1_value[0], data2_value, ldap_field, data1_keys)
-    elif data1_keys == "rdp.conf":
-        rdp_field = ["schema_type", "cookie", "cotp_connect_request", "cotp_connect_confirm", "rdp_negotiation_request",
-                     "rdp_negotiation_failure", "rdp_negotiation_response"]
-        rdp_field.extend(fields_to_compare)
-        comparative_data.field_data(data1_value[0], data2_value, rdp_field, data1_keys)
+                      "sys_contact", "sys_name", "sys_memory_size", "sys_if_number"],
+            "default_fields": fields_to_compare
+        },
+        "llmnr.conf": {
+            "fields": ["schema_type", "query", "qtype", "qtype_name", "rcode_name", "answers", "answer_queries", "rejected", "rcode",
+                       "answer_types"],
+            "default_fields": fields_to_compare
+        },
+        "ldap.conf": {
+            "fields": ["schema_type", "op", "req_dn", "res_code", "matched_dn", "res_error"],
+            "default_fields": fields_to_compare
+        },
+        "rdp.conf": {
+            "fields": ["schema_type", "cookie", "cotp_connect_request", "cotp_connect_confirm", "rdp_negotiation_request",
+                     "rdp_negotiation_failure", "rdp_negotiation_response"],
+            "default_fields": fields_to_compare
+        }
+    }
 
+    field_data = field_data_dict.get(data1_keys)
+    if field_data:
+        field_list = field_data["fields"] + field_data["default_fields"]
+        comparative_data.field_data(data1_value[0], data2_value, field_list, data1_keys)
 
 
 def Processing_data():
     data1, data2 = comparison_json_field()
     max_length = max(len(data1), len(data2))
+    # print(f"{data1}\n{data2}")
 
     key_data1 = []
     for i in range(max_length):
@@ -132,6 +149,7 @@ def Processing_data():
     # 清空结果文件
     is_clear = True
     Empty_file_content(is_clear, comparative_data_log)
+    Empty_file_content(is_clear, Comparison_results_log)
 
     for i in range(max_length):
         if i < len(data1):
